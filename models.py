353de02d76c18871b93104e29be349fe02aabdd6
vizal123 <yato9png@gmail.com>
2025-03-27 14:58:37 +0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+from flask_bcrypt import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password).decode('utf-8')
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
